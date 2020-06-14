6ae8232746c27476da82925e318eb709dba7b93f
Jon <synthetik023@gmail.com>
2020-06-14 16:59:28 -0400

    lsdk

--- fordave.js
+++ fordave.js
@@ -5,7 +5,7 @@ var dt = require('./myfirstmodule');
 const port = process.env.PORT || 3000;
 const address = process.argv[2];
 
-https.createServer.get ('',(req, res) => {
+https.createServer('',(req, res) => {
 	res.writeHead(200, {'Content-Type' : 'text/html'}),
 	res.write('<p>"The date and time are currently: " + dt.myDateTime()</p>'),
     res.end()
