b1d148b71f24327ca7f1e0fd6d026637463b936e
Jon <synthetik023@gmail.com>
2020-06-14 10:33:43 -0400

    this time

--- fordave.js
+++ fordave.js
@@ -3,11 +3,13 @@
 var https = require('https');
 var dt = require('./myfirstmodule');
 const port = process.env.PORT || 3000;
+const address = process.argv[2];
 
-https.createServer(function (req, res){
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	res.write("The date and time are currently: " + dt.myDateTime());
-	res.end();
+https.Server ((req, res) => {
+	res.writeHead(200, {'Content-Type' : 'text/html'}),
+	res.write("The date and time are currently: " + dt.myDateTime()),
+    res.end()
+    console.log(res.write);
 })
 .listen(port, ()=>{
     console.log('server is connected on port ' + port);
